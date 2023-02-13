3903db5778b7bc74b1c609ba4cac846e992a489f
Said Hadj-Larbi <said.hadjlarbi@bbox.fr>
2023-02-13 10:30:02 +0100

    Correction erreur tests unitaires dans FareCalculatorServiceTest : calculateFareBikeWithLessThanOneHourParkingTime et 			calculateFareCarWithLessThanOneHourParkingTime

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,11 +10,21 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
+
+        
         int inHour = ticket.getInTime().getHours();
         int outHour = ticket.getOutTime().getHours();
-
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        
+        int inMin = ticket.getInTime().getMinutes();
+        int outMin = ticket.getOutTime().getMinutes();   
+        
+        
+       
+        int durationHours = outHour - inHour;
+        float durationMins = (outMin - inMin)/60.0f;       
+        
+        
+        float duration =  durationHours + durationMins;
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
