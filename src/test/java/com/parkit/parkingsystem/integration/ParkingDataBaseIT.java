e35dfb8abf45fa30aae2b868596a336d3c255ae9
Said Hadj-Larbi <said.hadjlarbi@bbox.fr>
2023-02-13 11:56:31 +0100

    DONE : check that the fare generated and out time are populated correctly in the database

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -7,6 +7,7 @@ import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.service.FareCalculatorService;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -58,6 +59,7 @@ public class ParkingDataBaseIT {
     public void testParkingACar(){
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
+        
 
         Ticket ticket = ticketDAO.getTicket("ABCDEF");
 		assertNotNull(ticket);
@@ -75,7 +77,18 @@ public class ParkingDataBaseIT {
         testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
-        //TODO: check that the fare generated and out time are populated correctly in the database
+        
+        
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+		assertNotNull(ticket);
+
+		Double priceInDb = ticket.getPrice();
+
+		new FareCalculatorService().calculateFare(ticket);
+		
+		Double priceToBePaid = ticket.getPrice();
+
+		assertThat(priceInDb).isEqualTo(priceToBePaid);
     }
 
 }
